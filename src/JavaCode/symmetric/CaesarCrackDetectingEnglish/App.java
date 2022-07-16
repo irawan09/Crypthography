8895836fe8444079bee68db742d130fa77a2f45a
Ary Kurniadi Irawan <arykurniadiirawan@yahoo.co.id>
2022-07-16 13:41:41 +0200

    Update

---
+++ src/JavaCode/symmetric/CaesarCrackDetectingEnglish/App.java
@@ -0,0 +1,10 @@
+package JavaCode.symmetric.CaesarCrackDetectingEnglish;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        CaesarCipherBruteForce cracker = new CaesarCipherBruteForce();
+        cracker.crack("WKLVCLVCMXVWCDQCH DPSOH");
+    }
+}
